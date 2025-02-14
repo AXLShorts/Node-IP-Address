3325766c9381e5c8f35fa23b42f650939b65e87e
AXL <116296322+AXLShorts@users.noreply.github.com>
2025-02-14 14:18:49 +0000

    Done

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const checkIpAddress = require("../middleware/ipWhitelisting.middleware");
 
 const app = express();
+app.use(checkIpAddress);
 
 app.get("/", (req, res) => {
-  const ip1 = req.headers["x-forwarded-for"];
-  const ip2 = req.socket.remoteAddress;
-  const ip3 = req.ip;
-  res.send(`Your IP Is: ${ip1} ${ip2} ${ip3}`);
+  const ip = req.headers["x-forwarded-for"];
+  res.send(`Your IP Is: ${ip}`);
 });
 
 app.listen(3000, () => {
--- middleware/ipWhitelisting.middleware.js
+++ middleware/ipWhitelisting.middleware.js
@@ -1,32 +1,22 @@
-const allowedIpAddresses = [
-    "103.12.43.80"
-];
+const allowedIpAddresses = ["103.12.43.80"];
 
-export const checkIpAddress = async (
-  req,
-  res,
-  next
-) => {
+const checkIpAddress = (req, res, next) => {
   try {
     const ip = req.headers["x-forwarded-for"];
-    if (!ip) {
-      return res.status(403).send({
+    if (!ip || !allowedIpAddresses.includes(ip)) {
+        return res.status(403).json({
+          success: false,
+          error: { code: 403, message: "Access Denied." },
+        });
+      }
+  
+      next();
+    } catch (err) {
+      return res.status(500).json({
         success: false,
-        error: { code: 403, message: "Access Denied." },
+        error: { code: 500, message: err.message },
       });
     }
-
-    if (!allowedIpAddresses.includes(ip)) {
-      return res.status(403).send({
-        success: false,
-        error: { code: 403, message: "Access Denied." },
-      });
-    }
-    next();
-  } catch (err) {
-    return res.status(500).send({
-      success: false,
-      error: { code: 500, message: (err).message },
-    });
-  }
-};
+  };
+  
+  module.exports = checkIpAddress;
